40a389d323cd04e09c0e4f0d56bbcb15976e7394
CJ <khristos666@gmail.com>
2021-11-15 16:05:48 -0500

    Update Windows_Agent_Self-Healing_Manual_Deployment.bat

--- Agent_SelfHeal/Windows_Agent_Self-Healing_Manual_Deployment.bat
+++ Agent_SelfHeal/Windows_Agent_Self-Healing_Manual_Deployment.bat
@@ -6,7 +6,9 @@ REM.
 IF NOT EXIST C:\IT MD C:\IT
 IF NOT EXIST C:\IT\Scripts MD C:\IT\Scripts
 IF NOT EXIST C:\IT\Scripts\Tasks MD C:\IT\Scripts\Tasks
-CURL https://raw.githubusercontent.com/CW-Khristos/scripts/master/Agent_SelfHeal/AgentSelfHeal.ps1 -o C:\IT\Scripts\AgentSelfHeal.ps1
-CURL https://raw.githubusercontent.com/CW-Khristos/scripts/master/Agent_SelfHeal/N-able%20Windows%20Agent%20Self-Healing.xml -o "C:\IT\Scripts\Tasks\N-able Windows Agent Self-Healing.xml"
-SchTasks /CREATE /TN "IPM Computers\N-able Windows Agent Self-Healing" /XML "C:\IT\Scripts\Tasks\N-able Windows Agent Self-Healing.xml" /F
+CURL -k --ssl-no-revoke "https://raw.githubusercontent.com/CW-Khristos/scripts/master/Agent_SelfHeal/AgentSelfHeal.ps1" -o "C:\IT\Scripts\AgentSelfHeal.ps1"
+timeout 5
+CURL -k --ssl-no-revoke "https://raw.githubusercontent.com/CW-Khristos/scripts/dev/Agent_SelfHeal/N-able_Windows_Agent_Self-Healing.xml" -o "C:\IT\Scripts\Tasks\N-able_Windows_Agent_Self-Healing.xml"
+timeout 5
+SchTasks /CREATE /TN "IPM Computers\N-able Windows Agent Self-Healing" /XML "C:\IT\Scripts\Tasks\N-able_Windows_Agent_Self-Healing.xml" /F
 PowerShell -ExecutionPolicy Bypass -File C:\IT\Scripts\AgentSelfHeal.ps1
