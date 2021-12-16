de609baeecc0fac481567c1e39d25431a7f7f744
CJ <khristos666@gmail.com>
2021-12-16 14:22:08 -0500

    Update AVHealth_0.1.1.ps1

--- AVProducts/AVHealth_0.1.1.ps1
+++ AVProducts/AVHealth_0.1.1.ps1
@@ -99,7 +99,7 @@ function Get-AntiVirusProduct {
     $i = 0
     #COMMENT OUT THE BELOW LINE (LN92) FOR USE WITH AMP / PASSING OF PRIMARY AV AS INPUT
     #$i_PAV = "Sophos"
-    $srcAVP = "https://raw.githubusercontent.com/CW-Khristos/scripts/dev/AVProducts/" + $i_PAV.replace(" ", "").replace("-", "").tolower() + ".xml"
+    $srcAVP = "https://raw.githubusercontent.com/CW-Khristos/scripts/master/AVProducts/" + $i_PAV.replace(" ", "").replace("-", "").tolower() + ".xml"
     $srcAVP
     #READ AV PRODUCT DETAILS FROM XML
     try {
